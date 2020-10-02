a445d59801e7f5fcf845391dc45d08b5e9dcfba0
mujjazi <13besemmehdi@seecs.edu.pk>
2020-10-02 16:08:44 +0500

    Add project files.

---
+++ Automation-NUnit/Demo.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_NUnit
+{
+    class Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\mujta\\Desktop\\Selenium2\\");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Automation-NUnit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_NUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
